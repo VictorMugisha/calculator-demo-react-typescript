842830455ffd0e4479692306e5b333d156239a8f
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-30 10:41:47 +0200

    Created a Calculation Context Provider

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+import { CalculationContext } from "../contexts/CalculationContext";
 import Keyboard from "./Keyboard";
 import Screen from "./Screen";
 
 export default function Layout() {
+    const [expression, setExpression] = useState<string>('')
+
+    const calculationContextValue = {
+        expression,
+        setExpression
+    }
+
     return (
-        <>
+        <CalculationContext.Provider value={calculationContextValue}>
             <Screen />
             <Keyboard />
-        </>
+        </CalculationContext.Provider>
     )
 }
