b0f0fc5a4e461d4b250ba7578d546ae4afd774d1
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-30 10:53:35 +0200

    Implemented basic logic to show the flow of calculations

--- src/components/buttons/OperatorButton.tsx
+++ src/components/buttons/OperatorButton.tsx
@@ -1,12 +1,39 @@
+import { useCalculationContext } from "../../hooks/useCalculationContext";
+import { useScreenContext } from "../../hooks/useScreenContext";
 
 type OperatorButtonProps = {
-  operatorSymbol: string;
+  operatorSymbol: "÷" | "x" | "-" | "+";
 }
 
 export default function OperatorButton({ operatorSymbol }: OperatorButtonProps) {
+
+  const calculationContext = useCalculationContext()
+  const screenContext = useScreenContext()
+
+  function handleOperatorButtonClick() {
+    const expressionToMake: Array<number | string> = [screenContext.screenValue]
+    switch (operatorSymbol) {
+      case "+":
+        expressionToMake.push("+")
+        break
+      case "-":
+        expressionToMake.push("-")
+        break
+      case "x":
+        expressionToMake.push("*")
+        break
+      case "÷":
+        expressionToMake.push("/")
+        break
+    }
+
+    calculationContext.setExpression(expressionToMake.join(''))
+  }
+
   return (
     <button
       className="bg-orange-500 text-white border-none outline-none h-28 text-3xl md:text-4xl font-semibold"
+      onClick={handleOperatorButtonClick}
     >
       {operatorSymbol}
     </button>
