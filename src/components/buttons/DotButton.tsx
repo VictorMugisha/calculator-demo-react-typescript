a985540fadd55b284b48807e01e32bb04ad026a0
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-30 10:29:32 +0200

    Implemented basic functionalities of dotButton component

--- src/components/buttons/DotButton.tsx
+++ src/components/buttons/DotButton.tsx
@@ -1,6 +1,19 @@
+import { useScreenContext } from "../../hooks/useScreenContext"
 
 export default function DotButton() {
+  const screenContext = useScreenContext()
+  function handleDotButtonClick() {
+    if (screenContext.screenValue.toString().includes('.')) {
+      return
+    } else {
+    }
+  }
   return (
-    <button className="bg-gray-100 border-none  outline-none h-28 text-3xl md:text-4xl font-semibold">.</button>
+    <button
+      className="bg-gray-100 border-none  outline-none h-28 text-3xl md:text-4xl font-semibold"
+      onClick={handleDotButtonClick}
+    >
+      .
+    </button>
   )
 }
